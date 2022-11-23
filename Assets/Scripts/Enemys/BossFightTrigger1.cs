bf8eb082922d527053362f63ee145f7ceee3f08c
geropython <72422102+geropython@users.noreply.github.com>
2022-11-23 20:26:05 -0300

    lava move, map, boss

--- Assets/Scripts/Enemys/BossFightTrigger1.cs
+++ Assets/Scripts/Enemys/BossFightTrigger1.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class BossFightTrigger1 : MonoBehaviour
 {
     [SerializeField] GameObject wall;
-    [SerializeField] GameObject wall2;
-    [SerializeField] GameObject wall3;
-    [SerializeField] GameObject wall4;
     [SerializeField] BossScript boss;
     [SerializeField] GameObject bossLifebar;
 
