fc3421fb034e62196f83e095af45aaa5e5adbf36
laujuvi <laujuvi@hotmail.com>
2022-12-17 23:02:15 -0300

    Arreglo de script

--- Assets/transitionLevel.cs
+++ Assets/transitionLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class transitionLevel1 : MonoBehaviour
+public class transitionLevel : MonoBehaviour
 {
     //Tutorial Level1
         private void OnCollisionEnter(Collision collision)
